d94f82157987a5bc227ef01f221dcfc991bc04b1
JerryYuchang <pyuchang@qq.com>
2017-03-04 08:57:43 +0800

    add the function of displaying the message of weather

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by panyuchang on 2017/3/2.
+ */
+
+public class AQI {
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+
+    public AQICity city;
+}
